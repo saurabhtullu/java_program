19dbe0b6de480e51faa415d469c6030b4dd434a7
saurabhtullu <108823122+saurabhtullu@users.noreply.github.com>
2023-09-25 23:15:13 +0530

    Create basicUserInput.java
    Task
    In this challenge, you must read  integers from stdin and then print them to stdout. Each integer must be printed on a new line. To make the problem a little easier, a portion of the code is provided for you in the editor below.
    
    Input Format
    
    There are  lines of input, and each line contains a single integer.
    
    Sample Input
    
    42
    100
    125
    Sample Output
    
    42
    100
    125

---
+++ Basic/basicUserInput.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+       int b; 
+        Scanner a=new Scanner(System.in);
+        int c=a.nextInt();
+        for(int i=1;i<=c;i++){
+       b=a.nextInt();
+
+            System.out.println(b);
+    }
+        }
+}
